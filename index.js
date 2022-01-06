3f4d2c7e2f3dd0d50d549955925e7b3f5929bfe0
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2022-01-06 15:34:09 +0700

    feat: add analytics modules

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 require("dotenv").config();
 const { Client, Intents } = require("discord.js");
+const { Analytics } = require("./modules/analytics.js");
 const { Filters } = require("./modules/filters.js");
 const { Public } = require("./modules/public.js");
 const { Stickers } = require("./modules/stickers.js");
 const { Timeout } = require("./modules/timeout.js");
+const { PrismaClient }= require("@prisma/client");
+
+const intent = {
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+  ],
+};
+
 const main = () => {
-  const client = new Client({
-    intents: [
-      Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_MESSAGES,
-      Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    ],
-  });
+  const prisma = new PrismaClient();
+  const client = new Client(intent);
 
   client.on("ready", () => {
     console.info(`Logged in as ${client.user.tag}!`);
@@ -27,6 +33,8 @@ const main = () => {
     Filters(msg, client);
 
     Timeout(msg, client);
+
+    Analytics(msg, client, prisma)
   });
 
   client.login(process.env.TOKEN);
