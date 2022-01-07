5a75710eee88879d5bda6e7ce600e1350bf9dfc5
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2022-01-07 19:20:52 +0700

    feat: refactor timeout

--- modules/timeout.js
+++ modules/timeout.js
@@ -1,87 +1,66 @@
 require("dotenv").config();
 const { MessageEmbed } = require("discord.js");
 
-const { checkRoles, sendMsg } = require("./utility.js");
+const { checkRoles, sendMsg, splitMessages, getUserFromMention, checkPermission } = require("./utility");
 
-const Timeout = async (msg) => {
-  const split = msg.content.split(/ +/);
-  const command = split[0].toLowerCase();
-  const args = split.slice(1);
+const timeCalc = {
+  s: (1),
+  m: (60),
+  h: (60 * 60),
+  d: (60 * 60 * 24)
+};
 
-  if (msg.author.bot) return;
-  if (command === `${process.env.PREFIX}timeout`) {
+const Timeout = async (msg, client) => {
+  try {
+    const { command, args } = splitMessages(msg);
+
+    if (command !== 'timeout') return;
     if (!checkRoles(msg)) return;
 
-    let userId;
-    let user = msg.mentions.users.first();
+    const users = [];
+    for (let i = 2; i < args.length; i++) {
+      const data = args[i];
+      const user = await getUserFromMention(data, client);
+      if(!user || user.bot) continue; // bukan user
+      users.push(user);
+    }
 
-    if (!user) {
-      const splitted = msg.content.trim().split(" ");
-      userId = splitted[1];
+    if(users.length == 0) {
+      msg.reply("Please mention a user");
+      return;
+    }
 
-      if (!userId) {
-        userId = msg.author.id;
+    const timeType = args[0].slice(-1);
+    // Check format waktu apakah sudah sesuai
+    if(!['s', 'm', 'h', 'd'].includes(timeType)) {
+      msg.reply("Please set the time");
+      return;
+    } 
+
+    const duration = parseInt(args[0].slice(0, -1)) || 0;
+    const durationMs = Math.floor(duration  * (timeCalc[timeType] ?? 1) * 1000); // Covert duration to miliseconds
+    // check apakah durasi melebihi batas maksimum
+    if(durationMs > 2419200001 ) {
+      msg.reply("Maximal timeout is 28d");
+      return;
+    } 
+
+    for (const user of users) {
+      try {
+        const guildMember = await msg.guild.members.fetch(user.id);
+        if(!await checkPermission(msg, guildMember)) return;
+        const timeoutEmbed = new MessageEmbed()
+          .setColor("#992d22")
+          .setDescription(`<@${user.id}> has been timeout`);
+        await guildMember.timeout(durationMs, args[1]);
+        sendMsg(msg.channel, { embeds: [timeoutEmbed] });
+      } catch (error) {
+        console.error(error);
       }
-    } else {
-      userId = user.id;
     }
 
-    let member;
-
-    try {
-      member = await msg.guild.members.fetch(userId);
-    } catch (error) {
-      return msg.reply(":x: User not found");
-    }
-
-    if (!args.length) {
-      return msg.reply("Please mention the user & set the time");
-    }
-
-    if (args.length < 2) {
-      return msg.reply("Please set the time");
-    }
-
-    const dur = args[1];
-
-    const duration = parseInt(dur.slice(0, -1));
-    const unit = dur[dur.length - 1];
-
-    let time = new Date().getTime();
-
-    switch (unit) {
-      case "s":
-        time = duration * 1000;
-        break;
-
-      case "m":
-        time = duration * 1000 * 60;
-        break;
-
-      case "h":
-        time = duration * 1000 * 60 * 60;
-        break;
-
-      case "d":
-        time = duration * 1000 * 60 * 60 * 24;
-        break;
-    }
-
-    const reason = args.slice(2).join(" ") || "";
-
-    const timeoutEmbed = new MessageEmbed()
-      .setColor("#992d22")
-      .setDescription(`<@${member.id}> has been timeout`);
-
-    member.timeout(time, reason).catch((err) => {
-      if ((err = 50035)) {
-        msg.reply("Maximal timeout is 28d");
-      }
-    });
-
-    if (time < 2419200001) {
-      sendMsg(msg.channel, { embeds: [timeoutEmbed] });
-    }
+  } catch (error) {
+    console.error(error);
   }
 };
 
