ed8a3796f053f85f2c85c3a225651a66eb7a12db
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2022-01-09 21:34:30 +0700

    add: userInfo command

---
+++ commands/userinfo.command.js
@@ -0,0 +1,80 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+exports.command = new SlashCommandBuilder()
+    .setName('userinfo')
+    .setDescription('Views your info or other user info!')
+    .addUserOption(option => option.setName('target').setDescription('Select a user'));
+
+exports.execute = async (interaction) => {
+    try {
+        await interaction.deferReply();
+        let member, name, status, voice, memberRoles, customStatus;
+        const target = interaction.options.getUser('target');
+
+        member = interaction.member;
+        if(target) member = await interaction.guild.members.fetch(target);
+
+        name = member.displayName;
+        status = !member.presence ? "Offline" : member.presence.status;
+        voice = !member.voice.channelId ? "None" : `<#${member.voice.channelId}>`;
+        memberRoles = !member.roles ? "None" : member._roles.map((role) => `<@&${role}>`);
+
+        customStatus = 'None';
+        if(!member.presence && member.presence.activities[0]) {
+            customStatus = member.presence.activities[0].state;
+        }
+
+        const userInfoEmbed = new MessageEmbed()
+            .setColor("#992d22")
+            .setTitle(`${name}'s Informations.`)
+            .setThumbnail(member.displayAvatarURL({ dynamic: true }))
+            .addFields(
+                {
+                    name: "User Nickname",
+                    value: name,
+                    inline: true,
+                },
+                { name: "User ID", value: `${member.user.id}`, inline: true },
+                {
+                    name: "Status",
+                    value: status,
+                    inline: true,
+                },
+                {
+                    name: "In Voice",
+                    value: voice,
+                    inline: false,
+                },
+                {
+                    name: "Custom Status",
+                    value: `${customStatus}`,
+                    inline: false,
+                },
+                {
+                    name: `Roles (${memberRoles.length})`,
+                    value: `${memberRoles}`,
+                    inline: false,
+                },
+                {
+                    name: `Highest Role`,
+                    value: `<@&${member._roles[0]}>`,
+                    inline: false,
+                },
+                {
+                    name: `Account Created`,
+                    value: new Date(member.user.createdTimestamp).toUTCString(),
+                    inline: true,
+                },
+                {
+                    name: `Joined Created`,
+                    value: new Date(member.joinedAt).toUTCString(),
+                    inline: true,
+                }
+            )
+            .setTimestamp();
+        await interaction.editReply({ embeds: [userInfoEmbed] });
+    } catch (error) {
+        console.error(error);
+    }
+};
