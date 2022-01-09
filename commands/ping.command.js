29ddabdf16165b2bec8fb55b2f6f528d0ebf3ed3
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2022-01-09 16:31:56 +0700

    feat: change ping

--- commands/ping.command.js
+++ commands/ping.command.js
@@ -1,12 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-exports.commands = [
-    new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-    new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-];
-
 exports.command = new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!');
 
 exports.execute = async (interaction) => {
-    interaction.reply('Pong!');
+    interaction.reply({content:'Pong!', ephemeral: true});
 };
