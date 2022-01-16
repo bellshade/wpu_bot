a19de46ee7c73b6272e24d982a1c6685d46dcc1c
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2022-01-16 16:39:50 +0700

    feat: code cleanup

--- events/ready.event.js
+++ events/ready.event.js
@@ -5,16 +5,18 @@ module.exports = {
     execute(client) {
         console.info(`Logged in as ${client.user.tag}!`);
         console.info('Bot is ready!');
+
         const prisma = client.prisma;
         const pointAutomation = async () => {
             try {
                 const guild = client.guilds.cache.get(process.env.GUILD_ID); // jika bot tidak hanya berada di 1 guild, client.guilds akan memberi data lebih dari 1 data guild
                 const ketuaRole = guild.roles.cache.get(process.env.ROLE_KETUA);
                 const memberRole = ketuaRole.members.map((member) => member.user);
+                const dateInt = new Date().setDate(new Date().getDate() - 7);
                 for (const member of memberRole) {
-                    const dateInt = new Date().setDate(new Date().getDate() - 7);
+
+                    // Ambil hasil pertama yang sesuai kriteria
                     const lastMsg = await prisma.messages.findFirst({
-                        // Ambil hasil pertama yang sesuai kriteria
                         where: {
                             author_id: member.id, // id member
                             timestamp: {
@@ -26,6 +28,7 @@ module.exports = {
                             timestamp: 'desc', // mengurutkan waktu agar yang terahkir muncul paling pertama
                         },
                     });
+
                     let massCreateNew = {
                         ketua_id: member.id,
                         ketua_name: member.username,
